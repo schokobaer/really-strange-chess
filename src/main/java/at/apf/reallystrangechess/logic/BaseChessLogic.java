f06cd8e5655bb780ccb288be6edfd9b2b3cf3db7
schokobaer <andreas.peter.fend@gmail.com>
2020-12-16 17:56:11 +0100

    Implemented BaseChessLogic.isCheckmate, *.isPatt, *.getMoveableFields

--- src/main/java/at/apf/reallystrangechess/logic/BaseChessLogic.java
+++ src/main/java/at/apf/reallystrangechess/logic/BaseChessLogic.java
@@ -26,21 +26,6 @@ public class BaseChessLogic {
         return new Position(width, height);
     }
 
-    private List<BoardField> revertBoard(List<BoardField> board) {
-        Position dimension = getBoardDimensions(board);
-        return board.stream()
-                .map(f -> new BoardField(
-                        new Position(
-                                dimension.getX() - (f.getPosition().getY() - 1),
-                                dimension.getY() - (f.getPosition().getY() - 1)
-                        ),
-                        f.getColor(),
-                        f.getFigure(),
-                        f.getMine()
-                ))
-                .collect(Collectors.toList());
-    }
-
     private List<BoardField> getLauferMoveableFields(List<BoardField> b, BoardField field) {
         List<BoardField> moveableFields = new ArrayList<>();
         Position dimension = getBoardDimensions(b);
@@ -168,26 +153,40 @@ public class BaseChessLogic {
         Figure fig = field.getFigure();
         Position dim = getBoardDimensions(board);
 
-        List<BoardField> b = board;
-        if (fig.getColor() == Color.BLACK) {
-            b = revertBoard(board);
+        if (fig.getColor() == Color.WHITE) {
+            BoardField single = getField(board, field.getPosition().move(0, 1));
+            if (single.getColor() != BoardFieldColor.EMPTY && single.getFigure() == null) {
+                moveableFields.add(single);
+            }
+            if (field.getPosition().getY() <= 2) {
+                BoardField twostep = getField(board, field.getPosition().move(0, 2));
+                if (twostep.getColor() != BoardFieldColor.EMPTY && twostep.getFigure() == null) {
+                    moveableFields.add(twostep);
+                }
+            }
+        } else {
+            BoardField single = getField(board, field.getPosition().move(0, -1));
+            if (single.getColor() != BoardFieldColor.EMPTY && single.getFigure() == null) {
+                moveableFields.add(single);
+            }
+            if (field.getPosition().getY() >= dim.getY() - 2) {
+                BoardField twostep = getField(board, field.getPosition().move(0, -2));
+                if (twostep.getColor() != BoardFieldColor.EMPTY && twostep.getFigure() == null) {
+                    moveableFields.add(twostep);
+                }
+            }
         }
 
-        BoardField upLeft = getField(b, field.getPosition().move(-1, 1));
-        if (upLeft.getColor() != BoardFieldColor.EMPTY && upLeft.getFigure() != null && upLeft.getFigure().getColor() != fig.getColor()) {
-            moveableFields.add(upLeft);
-        }
-        BoardField upRight = getField(b, field.getPosition().move(1, 1));
-        if (upRight.getColor() != BoardFieldColor.EMPTY && upRight.getFigure() != null && upRight.getFigure().getColor() != fig.getColor()) {
-            moveableFields.add(upRight);
-        }
-
-        // Revert again
-        moveableFields = revertBoard(moveableFields);
 
         return moveableFields;
     }
 
+    /**
+     * Returns all fields the figure could hit a figure if at that field is a figure.
+     * @param board
+     * @param field
+     * @return
+     */
     private List<BoardField> getAllHitableFields(List<BoardField> board, BoardField field) {
         List<BoardField> moveableFields = new ArrayList<>();
 
@@ -196,125 +195,157 @@ public class BaseChessLogic {
         }
 
         Figure fig = field.getFigure();
-        Position dim = getBoardDimensions(board);
-
-        List<BoardField> b = board;
-        if (fig.getColor() == Color.BLACK) {
-            b = revertBoard(board);
-        }
 
         if (fig.getType() == FigureType.BAUER) {
-            BoardField up = getField(b, field.getPosition().move(0, 1));
-            if (up.getColor() != BoardFieldColor.EMPTY && up.getFigure() == null) {
-                moveableFields.add(up);
-            }
-            if (field.getPosition().getY() <= 2) {
-                BoardField up2 = getField(b, field.getPosition().move(0, 2));
-                if (up2.getColor() != BoardFieldColor.EMPTY && up2.getFigure() == null) {
-                    moveableFields.add(up2);
+            if (fig.getColor() == Color.WHITE) {
+                BoardField left = getField(board, field.getPosition().move(-1, 1));
+                if (left.getColor() != BoardFieldColor.EMPTY && left.getFigure() != null && left.getFigure().getColor() == Color.BLACK) {
+                    moveableFields.add(left);
+                }
+                BoardField right = getField(board, field.getPosition().move(1, 1));
+                if (right.getColor() != BoardFieldColor.EMPTY && right.getFigure() != null && right.getFigure().getColor() == Color.BLACK) {
+                    moveableFields.add(right);
+                }
+            } else {
+                BoardField left = getField(board, field.getPosition().move(-1, -1));
+                if (left.getColor() != BoardFieldColor.EMPTY && left.getFigure() != null && left.getFigure().getColor() == Color.WHITE) {
+                    moveableFields.add(left);
+                }
+                BoardField right = getField(board, field.getPosition().move(1, -1));
+                if (right.getColor() != BoardFieldColor.EMPTY && right.getFigure() != null && right.getFigure().getColor() == Color.WHITE) {
+                    moveableFields.add(right);
                 }
             }
         } else if (fig.getType() == FigureType.LAUEFER) {
-            moveableFields.addAll(getLauferMoveableFields(b, field));
+            moveableFields.addAll(getLauferMoveableFields(board, field));
         } else if (fig.getType() == FigureType.SPRINGER) {
-            BoardField f = getField(b, field.getPosition().move(-1, -2));
+            BoardField f = getField(board, field.getPosition().move(-1, -2));
             if (f.getColor() != BoardFieldColor.EMPTY && (f.getFigure() == null || f.getFigure().getColor() != fig.getColor())) {
                 moveableFields.add(f);
             }
-            f = getField(b, field.getPosition().move(1, -2));
+            f = getField(board, field.getPosition().move(1, -2));
             if (f.getColor() != BoardFieldColor.EMPTY && (f.getFigure() == null || f.getFigure().getColor() != fig.getColor())) {
                 moveableFields.add(f);
             }
-            f = getField(b, field.getPosition().move(2, -1));
+            f = getField(board, field.getPosition().move(2, -1));
             if (f.getColor() != BoardFieldColor.EMPTY && (f.getFigure() == null || f.getFigure().getColor() != fig.getColor())) {
                 moveableFields.add(f);
             }
-            f = getField(b, field.getPosition().move(2, 1));
+            f = getField(board, field.getPosition().move(2, 1));
             if (f.getColor() != BoardFieldColor.EMPTY && (f.getFigure() == null || f.getFigure().getColor() != fig.getColor())) {
                 moveableFields.add(f);
             }
-            f = getField(b, field.getPosition().move(1, 2));
+            f = getField(board, field.getPosition().move(1, 2));
             if (f.getColor() != BoardFieldColor.EMPTY && (f.getFigure() == null || f.getFigure().getColor() != fig.getColor())) {
                 moveableFields.add(f);
             }
-            f = getField(b, field.getPosition().move(-1, 2));
+            f = getField(board, field.getPosition().move(-1, 2));
             if (f.getColor() != BoardFieldColor.EMPTY && (f.getFigure() == null || f.getFigure().getColor() != fig.getColor())) {
                 moveableFields.add(f);
             }
-            f = getField(b, field.getPosition().move(-2, 1));
+            f = getField(board, field.getPosition().move(-2, 1));
             if (f.getColor() != BoardFieldColor.EMPTY && (f.getFigure() == null || f.getFigure().getColor() != fig.getColor())) {
                 moveableFields.add(f);
             }
-            f = getField(b, field.getPosition().move(-2, -1));
+            f = getField(board, field.getPosition().move(-2, -1));
             if (f.getColor() != BoardFieldColor.EMPTY && (f.getFigure() == null || f.getFigure().getColor() != fig.getColor())) {
                 moveableFields.add(f);
             }
         } else if (fig.getType() == FigureType.TURM) {
-            moveableFields.addAll(getTurmMoveableFields(b, field));
+            moveableFields.addAll(getTurmMoveableFields(board, field));
         } else if (fig.getType() == FigureType.DAME) {
-            moveableFields.addAll(getTurmMoveableFields(b, field));
-            moveableFields.addAll(getLauferMoveableFields(b, field));
+            moveableFields.addAll(getTurmMoveableFields(board, field));
+            moveableFields.addAll(getLauferMoveableFields(board, field));
         } else if (fig.getType() == FigureType.KING) {
-            BoardField f = getField(b, field.getPosition().move(-1, -1));
+            BoardField f = getField(board, field.getPosition().move(-1, -1));
             if (f.getColor() != BoardFieldColor.EMPTY && (f.getFigure() == null || f.getFigure().getColor() != fig.getColor())) {
                 moveableFields.add(f);
             }
-            f = getField(b, field.getPosition().move(0, -1));
+            f = getField(board, field.getPosition().move(0, -1));
             if (f.getColor() != BoardFieldColor.EMPTY && (f.getFigure() == null || f.getFigure().getColor() != fig.getColor())) {
                 moveableFields.add(f);
             }
-            f = getField(b, field.getPosition().move(1, -1));
+            f = getField(board, field.getPosition().move(1, -1));
             if (f.getColor() != BoardFieldColor.EMPTY && (f.getFigure() == null || f.getFigure().getColor() != fig.getColor())) {
                 moveableFields.add(f);
             }
-            f = getField(b, field.getPosition().move(-1, 0));
+            f = getField(board, field.getPosition().move(-1, 0));
             if (f.getColor() != BoardFieldColor.EMPTY && (f.getFigure() == null || f.getFigure().getColor() != fig.getColor())) {
                 moveableFields.add(f);
             }
-            f = getField(b, field.getPosition().move(1, 0));
+            f = getField(board, field.getPosition().move(1, 0));
             if (f.getColor() != BoardFieldColor.EMPTY && (f.getFigure() == null || f.getFigure().getColor() != fig.getColor())) {
                 moveableFields.add(f);
             }
-            f = getField(b, field.getPosition().move(-1, 1));
+            f = getField(board, field.getPosition().move(-1, 1));
             if (f.getColor() != BoardFieldColor.EMPTY && (f.getFigure() == null || f.getFigure().getColor() != fig.getColor())) {
                 moveableFields.add(f);
             }
-            f = getField(b, field.getPosition().move(0, 1));
+            f = getField(board, field.getPosition().move(0, 1));
             if (f.getColor() != BoardFieldColor.EMPTY && (f.getFigure() == null || f.getFigure().getColor() != fig.getColor())) {
                 moveableFields.add(f);
             }
-            f = getField(b, field.getPosition().move(1, 1));
+            f = getField(board, field.getPosition().move(1, 1));
             if (f.getColor() != BoardFieldColor.EMPTY && (f.getFigure() == null || f.getFigure().getColor() != fig.getColor())) {
                 moveableFields.add(f);
             }
         }
 
-        // Revert again
-        moveableFields = revertBoard(moveableFields);
-
         return moveableFields;
     }
 
-    private boolean isCheck(List<BoardField> board, Color color) {
-        BoardField kingField = board.stream()
+    public boolean isCheck(List<BoardField> board, Color color) {
+        return board.stream()
                 .filter(f -> f.getFigure() != null && f.getFigure().getColor() == color
                         && f.getFigure().getType() == FigureType.KING)
-                .findFirst().get();
-
-        return board.stream()
-                .filter(f -> f.getFigure() != null && f.getFigure().getColor() == color.flip()) // get all oponent figures
-                .map(f -> getAllHitableFields(board, f))
-                .flatMap(f -> f.stream())
-                .filter(f -> f.getPosition().equals(kingField.getPosition()))
-                .findAny().isPresent();
+                .anyMatch(kingField -> board.stream()
+                        .filter(f -> f.getFigure() != null && f.getFigure().getColor() == color.flip()) // get all oponent figures
+                        .map(f -> getAllHitableFields(board, f))
+                        .flatMap(f -> f.stream())
+                        .filter(f -> f.getPosition().equals(kingField.getPosition()))
+                        .findAny().isPresent());
     }
 
-    private List<BoardField> move(List<BoardField> board, Position from, Position to) {
+    private boolean canMakeMoveWithoutBeeingInCheck(List<BoardField> board, Color color) {
+        return board.stream()
+                .filter(f -> f.getFigure() != null && f.getFigure().getColor() == color) // get all figures of color
+                .anyMatch(figField -> !getMoveableFields(board, figField, true).isEmpty()) // check if there exist one figure, that can make a move
+                ;
+    }
+
+    public boolean isPatt(List<BoardField> board, Color color) {
+        return getCheckKings(board, color).isEmpty() && !canMakeMoveWithoutBeeingInCheck(board, color);
+    }
+
+    public boolean isCheckmate(List<BoardField> board, Color color) {
+        return !getCheckKings(board, color).isEmpty() && !canMakeMoveWithoutBeeingInCheck(board, color);
+    }
+
+    public List<BoardField> getCheckKings(List<BoardField> board, Color color) {
+        return board.stream()
+                .filter(f -> f.getFigure() != null && f.getFigure().getColor() == color
+                        && f.getFigure().getType() == FigureType.KING)
+                .filter(kingField -> board.stream()
+                        .filter(f -> f.getFigure() != null && f.getFigure().getColor() == color.flip()) // get all oponent figures
+                        .map(f -> getAllHitableFields(board, f))
+                        .flatMap(f -> f.stream())
+                        .filter(f -> f.getPosition().equals(kingField.getPosition()))
+                        .findAny().isPresent())
+                .collect(Collectors.toList());
+    }
+
+    public List<BoardField> move(List<BoardField> board, Position from, Position to) {
         Figure fig = board.stream().filter(f -> f.getPosition().equals(from)).findFirst().orElse(new BoardField(from)).getFigure();
+        Position dim = getBoardDimensions(board);
         return board.stream().map(f -> {
             if (f.getPosition().equals(from)) {
                 return new BoardField(f.getPosition(), f.getColor(), null, f.getMine());
             } else if (f.getPosition().equals(to)) {
+                if (fig.getType() == FigureType.BAUER &&
+                        (fig.getColor() == Color.WHITE && to.getY() == dim.getY()
+                         || fig.getColor() == Color.BLACK && to.getY() == 1)) {
+                    fig.setType(FigureType.DAME);
+                }
                 return new BoardField(f.getPosition(), f.getColor(), fig, f.getMine());
             } else {
                 return f;
@@ -322,11 +353,18 @@ public class BaseChessLogic {
         }).collect(Collectors.toList());
     }
 
-    public List<BoardField> getMoveableFields(List<BoardField> board, BoardField field) {
-        // TODO: Implement
-        // castling
-        // isCheck checking for each BoardField
-        return getAllMoveableFields(board, field);
+    public List<BoardField> getMoveableFields(List<BoardField> board, BoardField field, boolean castlingable) {
+        if (field.getFigure() == null) {
+            return new ArrayList<>();
+        }
+        Figure fig = field.getFigure();
+
+        List<BoardField> moveable = getAllMoveableFields(board, field).stream().filter(to -> {
+            List<BoardField> postBoard = move(board, field.getPosition(), to.getPosition());
+            return !isCheck(postBoard, fig.getColor());
+        }).collect(Collectors.toList());
+        // TODO: Add castling
+        return moveable;
     }
 
 
--- src/test/java/at/apf/reallystrangechess/logic/LogicTest.java
+++ src/test/java/at/apf/reallystrangechess/logic/LogicTest.java
@@ -1,16 +1,40 @@
 package at.apf.reallystrangechess.logic;
 
-import at.apf.reallystrangechess.model.BoardField;
-import at.apf.reallystrangechess.model.BoardFieldColor;
-import at.apf.reallystrangechess.model.Color;
-import at.apf.reallystrangechess.model.Position;
+import at.apf.reallystrangechess.model.*;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LogicTest {
 
     private BaseChessLogic logic = new BaseChessLogic();
+
+    private List<BoardField> board4() {
+        List<BoardField> board = new ArrayList<>();
+        board.add(new BoardField(new Position(1, 1), BoardFieldColor.WHITE, null, null));
+        board.add(new BoardField(new Position(2, 1), BoardFieldColor.BLACK, null, null));
+        board.add(new BoardField(new Position(3, 1), BoardFieldColor.WHITE, null, null));
+        board.add(new BoardField(new Position(4, 1), BoardFieldColor.BLACK, null, null));
+
+        board.add(new BoardField(new Position(1, 2), BoardFieldColor.BLACK, null, null));
+        board.add(new BoardField(new Position(2, 2), BoardFieldColor.WHITE, null, null));
+        board.add(new BoardField(new Position(3, 2), BoardFieldColor.BLACK, null, null));
+        board.add(new BoardField(new Position(4, 2), BoardFieldColor.WHITE, null, null));
+
+        board.add(new BoardField(new Position(1, 3), BoardFieldColor.WHITE, null, null));
+        board.add(new BoardField(new Position(2, 3), BoardFieldColor.BLACK, null, null));
+        board.add(new BoardField(new Position(3, 3), BoardFieldColor.WHITE, null, null));
+        board.add(new BoardField(new Position(4, 3), BoardFieldColor.BLACK, null, null));
+
+        board.add(new BoardField(new Position(1, 4), BoardFieldColor.BLACK, null, null));
+        board.add(new BoardField(new Position(2, 4), BoardFieldColor.WHITE, null, null));
+        board.add(new BoardField(new Position(3, 4), BoardFieldColor.BLACK, null, null));
+        board.add(new BoardField(new Position(4, 4), BoardFieldColor.WHITE, null, null));
+
+        return board;
+    }
 
     public void printBoardColor(List<BoardField> board) {
         Position dim = logic.getBoardDimensions(board);
@@ -64,5 +88,52 @@ public class LogicTest {
         System.out.println();
         System.out.println("Figures Colors:");
         printBoardFigColors(board);
+    }
+
+    @Test
+    public void testCheck() {
+        List<BoardField> board = board4();
+        board.stream().filter(f -> f.getPosition().equals(new Position(2, 1))).findFirst().get().setFigure(new Figure(Color.WHITE, FigureType.KING));
+        board.stream().filter(f -> f.getPosition().equals(new Position(4, 2))).findFirst().get().setFigure(new Figure(Color.BLACK, FigureType.LAUEFER));
+        board.stream().filter(f -> f.getPosition().equals(new Position(3, 3))).findFirst().get().setFigure(new Figure(Color.BLACK, FigureType.LAUEFER));
+
+        Assert.assertFalse(logic.isCheck(board, Color.WHITE));
+
+        logic.getMoveableFields(board, board.stream().filter(f -> f.getPosition().equals(new Position(2, 1))).findFirst().get(), true)
+                .stream().forEach(f -> System.out.println(f));
+        System.out.println();
+
+        board = logic.move(board, new Position(2, 1), new Position(1, 1));
+        Assert.assertTrue(logic.isCheck(board, Color.WHITE));
+
+        logic.getMoveableFields(board, board.stream().filter(f -> f.getPosition().equals(new Position(1, 1))).findFirst().get(), true)
+                .stream().forEach(f -> System.out.println(f));
+        System.out.println();
+
+        Assert.assertFalse(logic.isCheckmate(board, Color.WHITE));
+        Assert.assertFalse(logic.isPatt(board, Color.WHITE));
+
+        board = logic.move(board, new Position(1, 1), new Position(2, 1));
+        board.stream().filter(f -> f.getPosition().equals(new Position(4, 2))).findFirst().get().getFigure().setType(FigureType.DAME); // Rechter Laufer zu Dame
+        Assert.assertFalse(logic.isCheckmate(board, Color.WHITE));
+        Assert.assertTrue(logic.isPatt(board, Color.WHITE));
+
+        board = logic.move(board, new Position(4, 2), new Position(2, 2));
+        Assert.assertTrue(logic.isCheckmate(board, Color.WHITE));
+        Assert.assertFalse(logic.isPatt(board, Color.WHITE));
+    }
+
+    @Test
+    public void someMoves() {
+        List<BoardField> board = logic.generateBoard();
+        printBoardFigs(board);
+        System.out.println("-----------------");
+
+        board = logic.move(board, new Position(5,2), new Position(5,4));
+        printBoardFigs(board);
+        System.out.println("-----------------");
+
+        logic.getMoveableFields(board, board.stream().filter(f -> f.getPosition().equals(new Position(5, 4))).findFirst().get(), false)
+                .stream().forEach(f -> System.out.println(f.getPosition()));
     }
 }
